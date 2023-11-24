37abfdfd944371b16712b9149f3f2bc1e6744a86
Sami-Ullah-Tufail <sami.u.tufail@gmail.com>
2023-11-25 02:23:56 +0500

    adding associations

--- person.rb
+++ person.rb
@@ -1,15 +1,15 @@
 require './nameable'
 
 class Person < Nameable
-  attr_reader :id
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
 
-  def initialize(age, name: 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def of_age?
@@ -22,5 +22,9 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def add_rental(_person, date)
+    Rental.new(date, book, self)
   end
 end
