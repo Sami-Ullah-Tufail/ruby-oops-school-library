fa3972d518e6c595f5c8359dca570f22d1d169a3
Sami Ullah Tufail <114190100+Sami-Ullah-Tufail@users.noreply.github.com>
2023-11-25 00:10:28 +0000

    making final changhs

--- person.rb
+++ person.rb
@@ -1,8 +1,7 @@
 require './nameable'
 
 class Person < Nameable
-  attr_reader :id, :rentals
-  attr_accessor :name, :age
+  attr_reader :id, :rentals, :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
